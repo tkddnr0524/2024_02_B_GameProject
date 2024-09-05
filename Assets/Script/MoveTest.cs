cddd5ed564fd346a7d55b203ecea063f954a9d65
tkddnr0524 <tkddnr0110@naver.com>
2024-09-05 10:03:57 +0900

    [Feat] 소스 코드 반영

--- Assets/Script/MoveTest.cs
+++ Assets/Script/MoveTest.cs
@@ -6,6 +6,7 @@ public class MoveTest : MonoBehaviour
 {
     public Vector3 MoveOffset;
     public Vector3 SpeedOffset;
+    public Vector3 JumpOffset;
     // Start is called before the first frame update
     void Start()
     {
