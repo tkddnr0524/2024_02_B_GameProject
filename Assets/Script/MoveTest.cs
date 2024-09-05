e76ac5ed42e1b74c614e92f261b87406621a0bd4
tkddnr0524 <tkddnr0110@naver.com>
2024-09-05 09:54:12 +0900

    [Feat] 속도 변수 생성

--- Assets/Script/MoveTest.cs
+++ Assets/Script/MoveTest.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class MoveTest : MonoBehaviour
 {
-    public Vector3 MoveOffset; 
+    public Vector3 MoveOffset;
+    public Vector3 SpeedOffset;
     // Start is called before the first frame update
     void Start()
     {
